fd7153d9fd5ee468cecb900cb665d4f92e208927
Constantin Gavrilescu <costi@apluscompuservices.com>
2009-09-25 00:57:49 -0500

    created basic site layout and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,22 +8,18 @@ module ApplicationHelper
     title
   end
   
-  # this function adds the class opened to the menu item that is active
-  def menu_items_with_class
-    menu_items.map do |link|
-      link[:class] = (request.path =~ /#{link[:address]}/) ? 'opened' : 'notOpened'
-      link
-    end
-  end
 
   def menu_items
     [
-      {:address => '/index.html', :text => 'HOME'},
-      {:address => '/page2.html', :text => 'page 2'},
-      {:address => '/page3.html', :text => 'page 3'},
-      {:address => '/page4.html', :text => 'page 4'},
-      {:address => '/page5.html', :text => 'page 5'},
-      {:address => new_contact_path, :text => 'CONTACT US'}
-    ]
+      { :address => '/', :text => 'HOME' },
+      { :address => '/page2.html', :text => 'page 2' },
+      { :address => '/page3.html', :text => 'page 3' },
+      { :address => '/page4.html', :text => 'page 4' },
+      { :address => '/page5.html', :text => 'page 5' },
+      { :address => '/page6.html', :text => 'CONTACT US' }
+    ].map do |link|   # this adds the class opened to the menu item that is active
+      link[:class] = (request.path =~ /^#{link[:address]}$/) ? 'opened' : 'notOpened'
+      link
+    end
   end
 end
