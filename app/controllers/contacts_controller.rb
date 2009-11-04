d6c1fafbe016b98d3ff07edc6e916f4dce397f74
Constantin Gavrilescu <costi@costi-personal.(none)>
2009-11-04 13:33:07 -0600

    Added contacts from gcl

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -25,16 +25,7 @@ class ContactsController < ApplicationController
   # GET /contacts/new.xml
   def new
     @contact = Contact.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @contact }
-    end
-  end
-
-  # GET /contacts/1/edit
-  def edit
-    @contact = Contact.find(params[:id])
+    @contact.subject = "Please contact me to discuss my project"
   end
 
   # POST /contacts
@@ -42,34 +33,15 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
 
-    respond_to do |format|
-      if @contact.save
-        flash[:notice] = 'Contact was successfully created.'
-        format.html { redirect_to(@contact) }
-        format.xml  { render :xml => @contact, :status => :created, :location => @contact }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @contact.errors, :status => :unprocessable_entity }
-      end
+    if @contact.save
+      ContactMailer.deliver_autoreply(@contact)
+      ContactMailer.deliver_notify_webmaster(@contact)
+      render 'message_sent'
+    else
+      render :action => "new" 
     end
   end
-
-  # PUT /contacts/1
-  # PUT /contacts/1.xml
-  def update
-    @contact = Contact.find(params[:id])
-
-    respond_to do |format|
-      if @contact.update_attributes(params[:contact])
-        flash[:notice] = 'Contact was successfully updated.'
-        format.html { redirect_to(@contact) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @contact.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+  
 
   # DELETE /contacts/1
   # DELETE /contacts/1.xml
